5933f56a0239196d9d19b24715cb6e8037ab3584
Денис <zololotarenko.2015@gmail.com>
2020-12-10 18:19:59 +0200

    added lib for parse JSON

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # stud-game
+
+## libraries
+
+* SFML - https://github.com/SFML/SFML
+* JSON - https://github.com/nlohmann/json
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,36 @@
 #include <SFML/Graphics.hpp>
+#include <nlohmann/json.hpp>
+#include "iostream"
+
+using json = nlohmann::json;
+using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+    json j;
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    j["test"] = 5;
+
+    cout << j.dump();
+
+//    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+//    sf::CircleShape shape(100.f);
+//    shape.setFillColor(sf::Color::Green);
+//
+//    while (window.isOpen())
+//    {
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//
+//        window.clear();
+//        window.draw(shape);
+//        window.display();
+//    }
 
     return 0;
 }
