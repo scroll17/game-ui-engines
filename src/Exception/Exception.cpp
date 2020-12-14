41dc73ab1045e62203ad58751330221ddde682b8
Денис <zololotarenko.2015@gmail.com>
2020-12-14 16:53:15 +0200

    added class for exception handling

--- src/Exception/Exception.cpp
+++ src/Exception/Exception.cpp
@@ -2,4 +2,26 @@
 // Created by user on 11.12.2020.
 //
 
-#include "Exeption.h"
+#include "Exception.h"
+
+static map<ExceptionType, string> EXCEPTION_DETAILS {
+  { ExceptionType::FileRead, "Error with read file." }
+};
+
+Exception::Exception(ExceptionType type, const string &message): m_error_type(type), m_custom_error_message(message) {
+
+}
+
+const ExceptionType &Exception::get_error_type() {
+    return m_error_type;
+}
+
+const string &Exception::get_error_message() {
+    if(m_custom_error_message.length() > 0) {
+        return m_custom_error_message;
+    } else {
+        return EXCEPTION_DETAILS[m_error_type];
+    }
+}
+
+
--- src/Exception/Exception.h
+++ src/Exception/Exception.h
@@ -2,13 +2,30 @@
 // Created by user on 11.12.2020.
 //
 
-#ifndef STUD_GAME_EXEPTION_H
-#define STUD_GAME_EXEPTION_H
+#ifndef STUD_GAME_EXCEPTION_H
+#define STUD_GAME_EXCEPTION_H
 
+#include "iostream"
+#include <map>
 
-class Exeption {
+using namespace std;
 
+enum class ExceptionType {
+    FileRead,
+    FileNotExist
 };
 
+class Exception {
+    private:
+        ExceptionType m_error_type;
+        const string &m_custom_error_message;
 
-#endif //STUD_GAME_EXEPTION_H
+    public:
+        explicit Exception(ExceptionType type, const string &message = "");
+        ~Exception() = default;
+
+        const ExceptionType& get_error_type();
+        const string& get_error_message();
+};
+
+#endif //STUD_GAME_EXCEPTION_H
