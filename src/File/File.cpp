a64a53b6e8316971514ff8afbc44e3b8819cbe35
Денис <zololotarenko.2015@gmail.com>
2021-01-03 19:38:37 +0200

    updated File class

--- src/File/File.cpp
+++ src/File/File.cpp
@@ -3,3 +3,64 @@
 //
 
 #include "File.h"
+
+File::File(string file_path) {
+    m_file_path = File::resolve_path(Constants::directory_path, std::move(file_path));
+}
+
+bool File::check_file_exists() const {
+    if(m_file_path.length() == 0) {
+        return false;
+    }
+
+    std::ifstream file;
+    file.open(m_file_path);
+
+    bool is_open = file.is_open();
+
+    file.close();
+
+    return is_open;
+}
+
+const string& File::get_file_path() const {
+    return m_file_path;
+}
+
+string File::resolve_path(string left, string right) {
+    if(right.at(0) == '/') {
+        return right;
+    }
+
+    int pos = 0;
+    while (pos != -1) {
+        pos = right.find("../");
+        if(pos != -1) {
+            int remove_to = left.rfind('/');
+            if(remove_to != 0) {
+                left = left.substr(0, remove_to);
+            }
+
+            right = right.substr(pos + 3);
+
+            continue;
+        }
+
+        pos = right.find("./");
+        if(pos != -1) {
+            right = right.substr(pos + 2);
+
+            continue;
+        }
+    }
+
+    return (left + "/" + right);
+}
+
+json File::to_json() const {
+    return json();
+}
+
+xml File::to_xml() const {
+    return xml();
+}
--- src/File/File.h
+++ src/File/File.h
@@ -5,65 +5,35 @@
 #ifndef STUD_GAME_FILE_H
 #define STUD_GAME_FILE_H
 
-#include <iostream>
 #include <fstream>
-#include <functional>
-#include <experimental/filesystem>
 #include <utility>
+
+#include "pugixml.hpp"
+#include <nlohmann/json.hpp>
 
 #include "../Constants/Constants.h"
 #include "../Exception/Exception.h"
 
 using namespace std;
-namespace fs = std::experimental::filesystem;
+
+using json = nlohmann::json;
+using xml = pugi::xml_document;
 
 class File {
     protected:
-        string m_file_path {};
+        string m_file_path;
+
+        virtual json to_json() const;
+        virtual xml to_xml() const;
 
     public:
-        explicit File(string file_path) {
-            m_file_path = File::resolve_path(Constants::directory_path, std::move(file_path));
-        }
+        explicit File(string file_path);
+        ~File() = default;
 
-        virtual bool check_file_exists() {
-            if(m_file_path.length() == 0) {
-                return false;
-            }
+        bool check_file_exists() const;
+        const string& get_file_path() const;
 
-            return fs::exists(m_file_path);
-        }
-
-        virtual const string& get_file_path() {
-            return m_file_path;
-        }
-
-        static string resolve_path(string left, string right) {
-            int pos = 0;
-            while (pos != -1) {
-                pos = right.find("../");
-                if(pos != -1) {
-                    int remove_to = left.rfind('/');
-                    if(remove_to != 0) {
-                        left = left.substr(0, remove_to);
-                    }
-
-                    right = right.substr(pos + 3);
-
-                    continue;
-                }
-
-                pos = right.find("./");
-                if(pos != -1) {
-                    right = right.substr(pos + 2);
-
-                    continue;
-                }
-            }
-
-            return (left + "/" + right);
-        }
+        static string resolve_path(string left, string right);
 };
-
 
 #endif //STUD_GAME_FILE_H
