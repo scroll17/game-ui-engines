75a89b350297f322c8547ff49feb35be7edf1668
Денис <zololotarenko.2015@gmail.com>
2021-01-13 18:57:10 +0200

    implement Time class

--- src/Time/Time.cpp
+++ src/Time/Time.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "Time.h"
+
+void Timer::reset() {
+    m_beg = clock_t::now();
+}
+
+double Timer::elapsed() const {
+    return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+}
--- src/Time/Time.h
+++ src/Time/Time.h
@@ -5,9 +5,21 @@
 #ifndef STUD_GAME_TIME_H
 #define STUD_GAME_TIME_H
 
+#include <chrono> // chrono
 
-class Time {
+class Timer {
+    private:
+        using clock_t = std::chrono::high_resolution_clock;
+        using second_t = std::chrono::duration<double, std::ratio<1> >;
 
+        std::chrono::time_point<clock_t> m_beg;
+
+    public:
+        Timer(): m_beg(clock_t::now()) {};
+
+        void reset();
+
+        double elapsed() const;
 };
 
 
