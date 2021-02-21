2edafc624b1dda80864025b1f835a4f4e89361f4
Denis Zolotarenko <zololotarenko.2015@gmail.com>
2021-02-21 17:05:09 +0200

    implemented fundamental type Text

--- src/Game/Interface/Types/Text/Text.cpp
+++ src/Game/Interface/Types/Text/Text.cpp
@@ -3,3 +3,88 @@
 //
 
 #include "Text.h"
+
+Text::Text(const string& value): Element({ 0, 0 }, { 0, 0 }) {
+    init(value);
+}
+
+Text::Text(const string& value, sf::Font& font, sf::Color& color): Element({ 0, 0 }, { 0, 0 }), m_font(font), m_color(color) {
+    init(value);
+}
+
+Text::~Text() {
+    delete m_value;
+}
+
+// PUBLIC VOID
+void Text::draw(sf::RenderWindow& window) {
+    if(m_need_build) throw Exception(Exception::ElementNotBuild);
+
+    window.draw(*m_value);
+}
+
+// PUBLIC SET
+Text& Text::build() {
+    if(!m_need_build) return (*this);;
+
+    m_value->setFont(m_font);
+    m_value->setFillColor(m_color);
+
+    m_value->setCharacterSize(m_text_size);
+
+    m_value->setOutlineColor(m_border_color);
+    m_value->setOutlineThickness(m_border_width);
+
+    m_value->setPosition(this->get_position());
+    m_value->setOrigin(this->get_origin());
+
+    this->actualize_size();
+
+    turn_on_building();
+    return (*this);
+}
+
+Text& Text::set_text(const string& str) {
+    m_value->setString(sf::String::fromUtf8(str.begin(), str.end()));
+    actualize_size();
+
+    return (*this);
+}
+
+Text& Text::set_text_size(int size) {
+    m_text_size = size;
+
+    turn_off_building();
+    return (*this);
+}
+
+Text& Text::set_font(const sf::Font& font) {
+    m_font = font;
+
+    turn_off_building();
+    return (*this);
+}
+
+Text& Text::set_color(const sf::Color& color) {
+    m_color = color;
+
+    turn_off_building();
+    return (*this);
+}
+
+// PROTECTED SET
+void Text::init(const string& str) {
+    if(m_value != nullptr) {
+        throw std::runtime_error("Already init");
+    }
+
+    m_value = new sf::Text;
+    m_value->setString(sf::String::fromUtf8(str.begin(), str.end()));
+
+    this->build();
+}
+
+void Text::actualize_size() {
+    auto bounds = m_value->getLocalBounds();
+    this->set_size({ bounds.width, bounds.height });
+}
--- src/Game/Interface/Types/Text/Text.h
+++ src/Game/Interface/Types/Text/Text.h
@@ -5,9 +5,42 @@
 #ifndef STUD_GAME_TEXT_H
 #define STUD_GAME_TEXT_H
 
+#include <string>
+#include <SFML/Graphics.hpp>
 
-class Text {
+#include "../Element/Element.h"
+#include "../../../Game.h"
 
+class Text: public Element {
+    private:
+        using Element::set_size;
+        using Element::set_width;
+        using Element::set_height;
+
+    protected:
+        int m_text_size { 18 };
+
+        sf::Font& m_font { Game::get_game_font() };
+        sf::Color m_color { sf::Color::Black };
+
+        sf::Text *m_value { nullptr };
+
+        void init(const string& value);
+        void actualize_size();
+
+    public:
+        explicit Text(const string& value);
+        explicit Text(const string& value, sf::Font& font, sf::Color& color);
+        ~Text() override;
+
+        virtual Text& build() override;
+        virtual void draw(sf::RenderWindow& window) override;
+
+        Text& set_text(const string& str);
+        Text& set_text_size(int size);
+
+        Text& set_font(const sf::Font& font);
+        Text& set_color(const sf::Color& color);
 };
 
 
