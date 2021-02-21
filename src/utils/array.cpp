bc4a0daa28aa6c11fcae8dcdf13ce5e9b8d4c12a
Denis Zolotarenko <zololotarenko.2015@gmail.com>
2021-02-21 16:05:44 +0200

    added bubble sort; added utils for work with array

--- src/utils/array.cpp
+++ src/utils/array.cpp
@@ -3,3 +3,64 @@
 //
 
 #include "array.h"
+
+extern int *bubble_sort(int *arr, size_t arr_size, size_t start, size_t stop) {
+    if(stop == 0) {
+        stop = arr_size;
+    }
+
+    size_t sort_to = stop;
+    while (sort_to > 1) {
+        size_t curr_index = start + 1;
+
+        while (curr_index < sort_to) {
+            size_t prev_index = curr_index - 1;
+
+            if(arr[prev_index] > arr[curr_index]) {
+                std::swap(arr[prev_index], arr[curr_index]);
+            }
+
+            curr_index++;
+        }
+
+        sort_to -= 1;
+    }
+
+    return arr;
+}
+
+extern void show_array(int *arr, size_t arr_size) {
+    std::cout << "[";
+
+    for(size_t i = 0; i < arr_size; i++) {
+        std::cout << arr[i];
+
+        if(i < (arr_size - 1)) {
+            std::cout << ",";
+        }
+    }
+
+    std::cout << "]";
+}
+
+
+extern int gen_random(size_t start, size_t end) {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+
+    std::uniform_int_distribution<int> distribution(start, end);
+
+    return distribution(mt);
+}
+
+extern int *generate_arr(size_t size, size_t range) {
+    int *arr = new int[size];
+
+    for(int i = 0; i < size; i++) {
+        int val = gen_random(0, range);
+
+        arr[i] = val;
+    }
+
+    return arr;
+}
--- src/utils/array.h
+++ src/utils/array.h
@@ -5,10 +5,17 @@
 #ifndef STUD_GAME_ARRAY_H
 #define STUD_GAME_ARRAY_H
 
+#include <glob.h>   // size_t
+#include <utility>  // swap
+#include <random>   // random_device, mt19937, uniform_int_distribution
+#include <iostream>
 
-class array {
+int *bubble_sort(int *arr, size_t arr_size, size_t start = 0, size_t stop = 0);
 
-};
+int *generate_arr(size_t, size_t);
 
+void show_array(int *arr, size_t size);
+
+int gen_random();
 
 #endif //STUD_GAME_ARRAY_H
