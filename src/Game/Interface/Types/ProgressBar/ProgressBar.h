0e6824865c63d40214ffccb2656115cefda5243d
Denis Zolotarenko <zololotarenko.2015@gmail.com>
2021-02-21 17:07:41 +0200

    init ProgressBar class

---
+++ src/Game/Interface/Types/ProgressBar/ProgressBar.h
@@ -0,0 +1,24 @@
+//
+// Created by user on 09.02.2021.
+//
+
+#ifndef STUD_GAME_PROGRESSBAR_H
+#define STUD_GAME_PROGRESSBAR_H
+
+
+class ProgressBar {
+    private:
+        int m_step;
+
+        int m_start;
+        int m_stop;
+
+    public:
+        ProgressBar(int step, int start = 0, int stop = 100): m_step(step), m_start(start), m_stop(stop) {};
+        ~ProgressBar();
+
+
+};
+
+
+#endif //STUD_GAME_PROGRESSBAR_H
