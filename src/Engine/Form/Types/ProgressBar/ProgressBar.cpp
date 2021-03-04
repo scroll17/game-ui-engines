ca501b0de4572b35f754871b388d607f46c88072
Денис <zololotarenko.2015@gmail.com>
2021-03-04 15:39:42 +0200

    added new Form Type ProgressBar

--- src/Engine/Form/Types/ProgressBar/ProgressBar.cpp
+++ src/Engine/Form/Types/ProgressBar/ProgressBar.cpp
@@ -3,3 +3,99 @@
 //
 
 #include "ProgressBar.h"
+
+ProgressBar::ProgressBar(const sf::Vector2f& size, float step_count):
+    Element({ 0, size.y }),
+    m_curr_step(0.f),
+    m_step_count(step_count),
+    m_step_in_pixels(size.x / step_count),
+    m_original_width(size.x)
+{
+    m_rectangle = new sf::RectangleShape();
+}
+
+ProgressBar::ProgressBar(float height, float start_width, float end_width, float step_count):
+   Element({ start_width, height }),
+   m_curr_step(0.f),
+   m_step_count(step_count),
+   m_step_in_pixels((end_width - start_width) / step_count),
+   m_original_width(end_width - start_width)
+{
+   m_rectangle = new sf::RectangleShape();
+}
+
+ProgressBar::~ProgressBar() {
+    delete m_rectangle;
+}
+
+/// PROTECTED GET
+bool ProgressBar::is_end() const {
+    return m_step_count == m_curr_step;
+}
+
+/// PROTECTED SET
+ProgressBar& ProgressBar::build() {
+    if(!m_need_build) return (*this);
+
+    m_rectangle->setSize(this->get_size());
+    m_rectangle->setFillColor(m_color);
+
+    m_rectangle->setOutlineThickness(m_border_width);
+    m_rectangle->setOutlineColor(m_border_color);
+
+    m_rectangle->setPosition(this->get_position());
+    m_rectangle->setOrigin(this->get_origin());
+
+    this->turn_on_building();
+    return (*this);
+}
+
+/// PUBLIC VOID
+void ProgressBar::draw(sf::RenderWindow& window) const {
+    window.draw(*m_rectangle);
+}
+
+/// PUBLIC SET
+ProgressBar& ProgressBar::set_color(const sf::Color& color) {
+    m_color = color;
+
+    this->turn_off_building();
+    return (*this);
+}
+
+bool ProgressBar::next() {
+    if(this->is_end()) return false;
+
+    if((m_curr_step + 1) > m_step_count) {
+        float diff = m_step_count - m_curr_step;
+
+        m_curr_step += diff;
+        this->set_width(this->get_width() + (m_step_in_pixels * diff));
+    } else {
+        m_curr_step++;
+        this->set_width(this->get_width() + m_step_in_pixels);
+    }
+
+    this->build();
+    return (m_curr_step != m_step_count);
+}
+
+bool ProgressBar::next(float step) {
+    if(this->is_end()) return false;
+
+    float next_step = m_curr_step + step;
+    float diff = (next_step > m_step_count ? m_step_count : next_step) - m_curr_step;
+
+    m_curr_step += diff;
+    this->set_width(this->get_width() + (m_step_in_pixels * diff));
+
+    this->build();
+    return (m_curr_step != m_step_count);
+}
+
+void ProgressBar::ready() {
+    if(this->is_end()) return;
+
+    this->next(m_step_count - m_curr_step);
+}
+
--- src/Engine/Form/Types/ProgressBar/ProgressBar.h
+++ src/Engine/Form/Types/ProgressBar/ProgressBar.h
@@ -5,19 +5,39 @@
 #ifndef STUD_GAME_PROGRESSBAR_H
 #define STUD_GAME_PROGRESSBAR_H
 
+#include "../../../Exception/Exception.h"
+#include "../Element/Element.h"
 
-class ProgressBar final {
-    private:
-        int m_step;
+class ProgressBar: public Element {
+    protected:
+        using Element::set_size;
 
-        int m_start;
-        int m_stop;
+    protected:
+        float m_curr_step;
+        const float m_original_width, m_step_in_pixels, m_step_count;
+
+        sf::Color m_color { sf::Color::Blue };
+        sf::RectangleShape *m_rectangle { nullptr };
+
+        bool is_end() const;
+
+        virtual ProgressBar& build() override;
 
     public:
-        ProgressBar(int step, int start = 0, int stop = 100): m_step(step), m_start(start), m_stop(stop) {};
-        ~ProgressBar();
+        explicit ProgressBar(const sf::Vector2f& size, float step_count);
+        explicit ProgressBar(float height, float start_width, float end_width, float step_count);
+        ~ProgressBar() override;
 
+        virtual void draw(sf::RenderWindow& window) const override;
 
+        ProgressBar& set_color(const sf::Color& color);
+
+        bool next();
+        bool next(float step);
+
+        void ready();
+
+//        static void auto_progress(ProgressBar& bar, long seconds);
 };
 
 
