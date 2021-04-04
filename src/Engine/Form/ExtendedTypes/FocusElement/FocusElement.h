08f58e74088a7d9b2f9f56f8d1b0f7f22961a6b1
Денис <zololotarenko.2015@gmail.com>
2021-04-04 23:25:27 +0300

    added Form/ExtendedTypes; added class FocusElement to Form/ExtendedTypes;

--- src/Engine/Form/ExtendedTypes/FocusElement/FocusElement.h
+++ src/Engine/Form/ExtendedTypes/FocusElement/FocusElement.h
@@ -5,10 +5,14 @@
 #ifndef STUD_GAME_FOCUSELEMENT_H
 #define STUD_GAME_FOCUSELEMENT_H
 
+#include "../../Objects/FocusObject/FocusObject.h"
+#include "../../Types/Element/Element.h"
 
-class FocusElement {
-
+class FocusElement: public Element, public FocusObject {
+    public:
+        explicit FocusElement(const sf::Vector2f& size): Element(size) {};
+        explicit FocusElement(const sf::Vector2f& size, const sf::Vector2f& position): Element(size, position) {};
+        virtual ~FocusElement() = default;
 };
-
 
 #endif //STUD_GAME_FOCUSELEMENT_H
