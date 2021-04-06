67ae30eb0a819d55dd9a9c51297bfd2332f372a9
Денис <zololotarenko.2015@gmail.com>
2021-04-06 10:09:25 +0300

    added Objects/ActionObject

---
+++ src/Engine/Form/Objects/ActionObject/ActionObject.cpp
@@ -0,0 +1,133 @@
+//
+// Created by user on 05.04.2021.
+//
+
+#include "ActionObject.h"
+
+// PROTECTED GET
+ActionObject::t_callbacks& ActionObject::get_callbacks_by_action(const Action& action) {
+    switch (action) {
+        case Click: return m_on_click_callbacks;
+        case Hover: return m_on_hover_callbacks;
+        case Focus: return m_on_focus_callbacks;
+        default: {
+            throw std::runtime_error("Invalid argument");
+        }
+    }
+}
+
+// PROTECTED SET
+size_t ActionObject::set_callbacks_by_action(const Action &action, const t_callback &cb, const t_callback &after_cb) {
+    t_callbacks& callbacks(get_callbacks_by_action(action));
+
+    callbacks.push_back(cb);
+    size_t pos = callbacks.size() - 1;
+
+    callbacks.push_back(after_cb);
+
+    return pos / 2;
+}
+
+void ActionObject::call_callbacks(const Action& action) {
+    t_callbacks& callbacks(get_callbacks_by_action(action));
+
+    if(callbacks.empty()) return;
+
+    const size_t size = callbacks.size();
+    for(size_t i = 0; i < size; i += 2) {
+        auto& callback = callbacks.at(i);
+        callback(*this);
+    }
+}
+
+void ActionObject::call_after_callbacks(const Action& action) {
+    t_callbacks& callbacks(get_callbacks_by_action(action));
+
+    if(callbacks.empty()) return;
+
+    const size_t size = callbacks.size();
+    for(size_t i = 1; i < size; i += 2) {
+        auto& callback = callbacks.at(i);
+        callback(*this);
+    }
+}
+
+void ActionObject::remove_callback(const Action& action, size_t pos) {
+    if(pos % 2 != 0) throw std::runtime_error("Invalid argument");
+
+    t_callbacks& callbacks(get_callbacks_by_action(action));
+
+    if(pos > (callbacks.size() - 2)) {
+        throw Exception(Exception::NonExistentPosition);
+    }
+
+    callbacks.erase(callbacks.begin() + pos);               /// on callback
+    callbacks.erase(callbacks.begin() + pos + 1);   /// after callback
+}
+
+// PUBLIC SET
+size_t ActionObject::on_click(const ActionObject::t_callback& cb, const ActionObject::t_callback& after_cb) {
+    return this->set_callbacks_by_action(Action::Click, cb, after_cb);
+}
+
+size_t ActionObject::on_hover(const ActionObject::t_callback& cb, const ActionObject::t_callback& after_cb) {
+    return this->set_callbacks_by_action(Action::Hover, cb, after_cb);
+}
+
+size_t ActionObject::on_focus(const ActionObject::t_callback &cb, const ActionObject::t_callback &after_cb) {
+    return this->set_callbacks_by_action(Action::Focus, cb, after_cb);
+}
+
+ActionObject &ActionObject::set_focus() {
+    m_focus = true;
+    this->call_callbacks(Action::Focus);
+
+    return (*this);
+}
+
+ActionObject &ActionObject::delete_focus() {
+    m_focus = false;
+    this->call_after_callbacks(Action::Focus);
+
+    return (*this);
+}
+
+ActionObject& ActionObject::click() {
+    this->call_callbacks(Action::Click);
+    return (*this);
+}
+
+ActionObject& ActionObject::after_click() {
+    this->call_after_callbacks(Action::Click);
+    return (*this);
+}
+
+ActionObject& ActionObject::hover() {
+    this->call_callbacks(Action::Hover);
+    return (*this);
+}
+
+ActionObject& ActionObject::after_hover() {
+    this->call_after_callbacks(Action::Hover);
+    return (*this);
+}
+
+ActionObject& ActionObject::remove_click(size_t pos) {
+    remove_callback(Action::Click, pos * 2);
+    return (*this);
+}
+
+ActionObject& ActionObject::remove_hover(size_t pos) {
+    remove_callback(Action::Hover, pos * 2);
+    return (*this);
+}
+
+ActionObject& ActionObject::remove_focus(size_t pos) {
+    remove_callback(Action::Focus, pos * 2);
+    return (*this);
+}
+
+// PUBLIC GET
+bool ActionObject::is_focused() const {
+    return m_focus;
+}
---
+++ src/Engine/Form/Objects/ActionObject/ActionObject.h
@@ -0,0 +1,63 @@
+//
+// Created by user on 05.04.2021.
+//
+
+#ifndef STUD_GAME_ACTIONOBJECT_H
+#define STUD_GAME_ACTIONOBJECT_H
+
+#include <vector>     // vector
+#include <functional> // function
+
+#include "../../../Exception/Exception.h"
+
+class ActionObject {
+    using t_callback = std::function<void(ActionObject&)>;
+    using t_callbacks = std::vector<t_callback>;
+
+    public:
+        enum Action {
+            Click,
+            Hover,
+            Focus
+        };
+
+    protected:
+        bool m_focus = false;
+
+        t_callbacks m_on_click_callbacks {};
+        t_callbacks m_on_hover_callbacks {};
+        t_callbacks m_on_focus_callbacks {};
+
+        size_t set_callbacks_by_action(const Action& action, const t_callback& cb, const t_callback& after_cb);
+        t_callbacks& get_callbacks_by_action(const Action& action);
+
+        void call_callbacks(const Action& action);
+        void call_after_callbacks(const Action& action);
+        void remove_callback(const Action& action, size_t pos);
+
+    public:
+        ActionObject() = default;
+        virtual ~ActionObject() = default;
+
+        size_t on_click(const t_callback& cb, const t_callback& after_cb);
+        size_t on_hover(const t_callback& cb, const t_callback& after_cb);
+        size_t on_focus(const t_callback& cb, const t_callback& after_cb);
+
+        ActionObject& set_focus();
+        ActionObject& delete_focus();
+
+        ActionObject& click();
+        ActionObject& after_click();
+
+        ActionObject& hover();
+        ActionObject& after_hover();
+
+        ActionObject& remove_click(size_t pos);
+        ActionObject& remove_hover(size_t pos);
+        ActionObject& remove_focus(size_t pos);
+
+        bool is_focused() const;
+};
+
+
+#endif //STUD_GAME_ACTIONOBJECT_H
