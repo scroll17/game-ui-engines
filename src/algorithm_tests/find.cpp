67f991642cc24a25fff454c4f52166681bf641a4
Денис <zololotarenko.2015@gmail.com>
2021-05-25 17:28:08 +0300

    added find algorithm test

--- src/algorithm_tests/find.cpp
+++ src/algorithm_tests/find.cpp
@@ -2,13 +2,15 @@
 // Created by user on 08.02.2021.
 //
 
-#include "iostream"
+#include <iostream>
 #include <iomanip>
 
-#include "../DataTypes/index.h"
-#include "../utils/index.h"
+#include "../DataTypes/Time/Time.h"
+#include "../utils/string/string.h"
 
-char str[] = "ZZZ Обратите внимание на разницу между функцией strlen() и оператором sizeof. strlen() выводит количество символов до нуль-т"
+using namespace std;
+
+const string str = "ZZZ Обратите внимание на разницу между функцией strlen() и оператором sizeof. strlen() выводит количество символов до нуль-т"
              "ерминатора, тогда как оператор sizeof возвращает размер целого массива, независимо от того, что в нем находится. "
              "ерминатора, тогда как оператор sizeof возвращает размер целого массива, независимо от того, что в нем находится. "
              "ерминаторZа, тогда каZк оператор sZizeof возвращает размер Zцелого массива, независимо от того, что в нем находится. "
@@ -31,45 +33,75 @@ char str[] = "ZZZ Обратите внимание на разницу межд
              "ерминатора, тогдZZа как оператор sizeof возвращает размер целого маZссива, независимо от того, что в нем находится. "
              "ерминатора, тZогда как оператор sizeof возвращает размер целого массива, независимо от того, что в нем находится. ZZ2ZZ";
 
-using namespace std;
+void show_diff(double v_1, double v_2) {
+    double diff = v_2 - v_1;
 
-string str_2 = str;
+    if(diff < 0) {
+        cout << "Алгоритм #1 медленее #2 на "
+             << std::fixed << std::setprecision(12)
+             << std::abs(diff)
+             << endl;
+    } else {
+        cout << "Алгоритм #1 быстрее #2 на "
+             << std::fixed << std::setprecision(12)
+             << diff
+             << endl;
+    }
+}
 
 void find_algorithm_test() {
     double t_v_1, t_v_2;
 
     char sub_str[] = "ZZ2ZZ";
+    cout << "@Подстрока для поиска = " << sub_str << endl;
 
     data_types::Timer t1;
 
-    int result_1 = utils::string::find(str, sub_str);
+    int result_1 = utils::string::find(str.c_str(), sub_str);
     t_v_1 = t1.elapsed();
 
     t1.reset();
 
-    int result_2 = str_2.find(sub_str);
+    int result_2 = str.find(sub_str);
     t_v_2 = t1.elapsed();
 
-    std::cout << "#1: " << std::fixed << std::setprecision(12) << t_v_1 << "  result: " << result_1 << endl;
-    std::cout << "#2: " << std::fixed << std::setprecision(12) << t_v_2 << "  result: " << result_2 << endl;
+    std::cout << "#1: " << std::fixed << std::setprecision(12) << t_v_1 << " результат: " << result_1 << endl;
+    std::cout << "#2: " << std::fixed << std::setprecision(12) << t_v_2 << " результат: " << result_2 << endl;
+
+    show_diff(t_v_1, t_v_2);
 }
 
 void rfind_algorithm_test() {
     double t_v_1, t_v_2;
 
     char sub_str[] = "ZZZ";
+    cout << "@Подстрока для поиска = " << sub_str << endl;
 
     data_types::Timer t1;
 
-    int result_1 = utils::string::rfind(str, sub_str);
+    int result_1 = utils::string::rfind(str.c_str(), sub_str);
     t_v_1 = t1.elapsed();
 
     t1.reset();
 
-    int result_2 = str_2.rfind(sub_str);
+    int result_2 = str.rfind(sub_str);
     t_v_2 = t1.elapsed();
 
-    std::cout << "#1: " << std::fixed << std::setprecision(12) << t_v_1 << "  result: " << result_1 << endl;
-    std::cout << "#2: " << std::fixed << std::setprecision(12) << t_v_2 << "  result: " << result_2 << endl;
+    std::cout << "#1: " << std::fixed << std::setprecision(12) << t_v_1 << " результат: " << result_1 << endl;
+    std::cout << "#2: " << std::fixed << std::setprecision(12) << t_v_2 << " результат: " << result_2 << endl;
 
+    show_diff(t_v_1, t_v_2);
+}
+
+int main() {
+    cout << "Исходные данные: " << endl << str << "\n\n";
+
+    cout << "1. Поиск с начала строки: " << endl;
+    find_algorithm_test();
+    cout << "\n";
+
+    cout << "1. Поиск с конца строки: " << endl;
+    rfind_algorithm_test();
+
+    return EXIT_SUCCESS;
 }
