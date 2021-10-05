b47edd4acebc328776fa8ba52142e20311025363
Denis Z <zololotarenko.2015@gmail.com>
2021-10-05 20:17:53 +0300

    added Logger

---
+++ src/Logger/Logger.cpp
@@ -0,0 +1,40 @@
+//
+// Created by user on 05.10.21.
+//
+
+#include "Logger.h"
+
+// PUBLIC GET
+std::ostream& Logger::log() const {
+    return std::cout << this->get_log_type_str() << ": ";
+}
+
+// PUBLIC SET
+std::ostream& Logger::log_uniq(std::string key, std::string val) {
+    if(key != last_log_key) {
+        last_log_key = key;
+        return std::cout
+            << this->get_log_type_str()
+            << ": "
+            << key
+            << val
+            << std::endl;
+    }
+
+    return std::cout << "";
+}
+
+
+// PRIVATE GET
+std::string Logger::get_log_type_str() const {
+    std::string label;
+
+    switch(log_type) {
+        case DEBUG: label = "DEBUG"; break;
+        case INFO:  label = "INFO "; break;
+        case WARN:  label = "WARN "; break;
+        case ERROR: label = "ERROR"; break;
+    }
+
+    return label;
+}
---
+++ src/Logger/Logger.h
@@ -0,0 +1,34 @@
+//
+// Created by user on 05.10.21.
+//
+
+#ifndef STUD_GAME_LOGGER_H
+#define STUD_GAME_LOGGER_H
+
+#include "iostream"
+
+class Logger {
+    public:
+        enum typelog {
+            DEBUG,
+            INFO,
+            WARN,
+            ERROR
+        };
+
+    private:
+        typelog log_type;
+        std::string last_log_key;
+
+        std::string get_log_type_str() const;
+
+    public:
+        Logger(typelog type): log_type(type) {};
+        ~Logger() = default;
+
+        std::ostream& log() const;
+        std::ostream& log_uniq(std::string key, std::string val);
+};
+
+
+#endif //STUD_GAME_LOGGER_H
